8fa847b7a3fb2118479536623a12d7774d3c9e58
AStream26 <kumaravi113@gmail.com>
2021-06-07 03:14:05 +0530

    ordercontroll process improved

--- Backend/Controllers/Ordercontroller.js
+++ Backend/Controllers/Ordercontroller.js
@@ -2,7 +2,7 @@ const Order = require('../Modal/Ordermodal');
 const catchAsync= require('../util/catchAsync');
 const AppError = require('../util/Errorhandler');
 const Product  =require('../Modal/ProductModal');
-
+const Cart  = require('../Modal/cartModal')
 exports.orderrequest = catchAsync(async (req,res,next)=>{
    
   
@@ -10,22 +10,20 @@ exports.orderrequest = catchAsync(async (req,res,next)=>{
     const {OrderDetail} = req.body;
    // console.log(OrderDetail);
     const {orderItems,shippingAddress,paymentMethod,shippingPrice,totalPrice,taxPrice} = OrderDetail;
+    
+    if(!OrderDetail || orderItems.length === 0 || !paymentMethod || !shippingAddress  )
+    return next(new AppError('Order cannot be Placed !!!',400));
 
  await Promise.all(   orderItems.map(async (el)=>{
     const product = await Product.findById(el.product);
     if(el.quantity >product.countInStock){
         return next(new AppError(`Sorry But ${product.name} Item Quantity has changed`,404));
-    }
+    }    
 }));
 
     
   let log = console.log;
      
-  
-    if(!OrderDetail || orderItems.length === 0 || !paymentMethod || !shippingAddress  )
-    return next(new AppError('Order cannot be Placed !!!',400));
-    
-
 const order = await Order.create({
     orderItems,
     shippingAddress,
@@ -35,6 +33,25 @@ const order = await Order.create({
     taxPrice,
     shippingPrice
 });
+
+if(order){
+    // console.log(req.body);
+     
+if(order.orderItems.length==1){
+ 
+ let product = {product:order.orderItems[0].product};
+ let query = {_id:req.user.cartID};
+ const data = await Cart.updateOne(query,{$pull:{products:product}});
+}
+else{
+ const data = await Cart.updateOne(query,{$set:{products:[]}});
+
+}
+
+
+
+  
+ }
 
 
 
@@ -64,41 +81,62 @@ exports.getorder  = catchAsync(async(req,res,next)=>{
 exports.Pay  = catchAsync(async(req,res,next)=>{
        
     const order = await Order.findById(req.params.id);
-   //  console.log(order.user);
-    // console.log(req.user.id);
+  //  console.log(order.user);
+   //  console.log(req.user.cartID);
     
     if(order.user != req.user.id)
     return next(new AppError('Invalid Order request !!',400));
-
-    if(order){
-       // console.log(req.body);
-  await  Promise.all( order.orderItems.map(async (el,i)=>{
-    const product = await Product.findById(el.product);
-    product.countInStock = product.countInStock - el.quantity;
-   
-
-    await product.save();
     
-}));
-        order.isPaid = true;
-        order.paidAt = Date.now();
-        order.paymentResults = {
-            id:req.body.id,
-            status:req.body.status,
-            update_time:req.body.update_time,         
-            email_address:req.body.payer.email_address
+   
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResults = {
+        id:req.body.id,
+        status:req.body.status,
+        update_time:req.body.update_time,         
+        email_address:req.body.payer.email_address
 
 
-        }
-
-        const updatedorder = await order.save();
-        res.status(200).json({
-            status:'success',
-            updatedorder
-        });
     }
 
- 
+
+
+    const updatedorder = await order.save();
+
+
+    if(order){
+        // console.log(req.body);
+         
+    if(order.orderItems.length==1){
+     const product1 = await Product.findById(order.orderItems[0].product);
+     product1.countInStock = product1.countInStock - order.orderItems[0].quantity;
+     await product1.save();
+   
+    }
+    else{
+              
+          
+     await  Promise.all( order.orderItems.map(async (el,i)=>{
+         const product = await Product.findById(el.product);
+         product.countInStock = product.countInStock - el.quantity;
+         await product.save();
+         
+     }));
+    
+    }
+    
+    
+    
+      
+     }
+    
+
+   
+
+    res.status(200).json({
+        status:'success',
+        updatedorder
+    });
 });
 
 exports.Delivered  = catchAsync(async(req,res,next)=>{
